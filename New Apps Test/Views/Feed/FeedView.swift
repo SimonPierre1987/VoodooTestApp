def515b765865553edb2d67dd029bfd2a17a8dbe
Pierre Simon <pierre.simon.pro87@gmail.com>
2024-11-19 16:23:02 +0100

    Separate FeedView and FeedItem view

--- New Apps Test/Views/Feed/FeedView.swift
+++ New Apps Test/Views/Feed/FeedView.swift
@@ -64,43 +64,6 @@ struct FeedView: View {
     }
 }
 
-struct FeedItemView: View {
-
-    let sharedPhoto: SharedPhoto
-
-    var body: some View {
-        VStack {
-            switch sharedPhoto.contentSource {
-            case .url(let url):
-                AsyncImage(url: url)
-                    .frame(width: 350, height: 300)
-                    .clipShape(.rect(cornerRadius: 20))
-            case .image(let image):
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 350, height: 300)
-                    .clipShape(.rect(cornerRadius: 20))
-            case .embeddedAsset(let string):
-                Image(string)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 350, height: 300)
-                    .clipShape(.rect(cornerRadius: 20))
-            }
-
-            HStack {
-                Text("Photo shared by " + sharedPhoto.author.name)
-                Spacer()
-                Label("\(sharedPhoto.chatThread.messages.count) messages", systemImage: "message")
-            }
-            .font(.caption)
-            .foregroundStyle(.gray)
-        }
-        .padding(.horizontal)
-    }
-}
-
 #Preview {
     FeedView()
 }
