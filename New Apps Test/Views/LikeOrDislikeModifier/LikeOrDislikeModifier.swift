8ab8b0b2cf1b8d021dce40735cbcec2c808f71ca
Pierre Simon <pierre.simon.pro87@gmail.com>
2024-11-20 14:23:38 +0100

    On purpose I don't wait for the backend to satisfy the user visually. It's an optimistic behavior. Most of the time the backend will be satisfying the user desire (about likes) If the backend fail, the visual state will be reset and not corrupted

--- New Apps Test/Views/LikeOrDislikeModifier/LikeOrDislikeModifier.swift
+++ New Apps Test/Views/LikeOrDislikeModifier/LikeOrDislikeModifier.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+extension View {
+    func likeOrDislikePhoto(photo: Binding<SharedPhoto>, showLikeAction: Binding<Bool>, size: CGFloat) -> some View {
+        modifier(LikeOrDislikeModifier(size: size, photo: photo, showLikeAction: showLikeAction))
+    }
+}
+
 struct LikeOrDislikeModifier: ViewModifier {
     // MARK: Services
     let allPhotosService = AllPhotosService()
@@ -44,17 +50,25 @@ struct LikeOrDislikeModifier: ViewModifier {
 private extension LikeOrDislikeModifier {
     private func likeOrDislikePhoto() async {
         if self.photo.isLikedByUser {
+            // On purpose I don't wait for the backend to satisfy the user visually.
+            // It's an optimistic behavior. Most of the time the backend will be satisfying the user desire (about likes)
+            // If the backend fails, the visual state will be reset and not corrupted
+            self.photo.isLikedByUser = false
+            withAnimation {
+                self.showLikeAction = true
+            }
             let dislikedPhoto = await self.allPhotosService.dislike(photo: self.photo)
-            self.photo = SharedPhoto(imageDTO: dislikedPhoto, chatThread: self.photo.chatThread)
-            withAnimation {
-                self.showLikeAction = true
-            }
+            self.photo.isLikedByUser = dislikedPhoto.likedByUser ?? false
         } else {
-            let photoLiked = await self.allPhotosService.like(photo: self.photo)
-            self.photo = SharedPhoto(imageDTO: photoLiked, chatThread: self.photo.chatThread)
+            // On purpose I don't wait for the backend to satisfy the user visually.
+            // It's an optimistic behavior. Most of the time the backend will be satisfying the user desire (about likes)
+            // if the backend fails, the visual state will be reset and not corrupted
+            self.photo.isLikedByUser = true
             withAnimation {
                 self.showLikeAction = true
             }
+            let photoLiked = await self.allPhotosService.like(photo: self.photo)
+            self.photo.isLikedByUser = photoLiked.likedByUser ?? false
         }
     }
 
@@ -70,8 +84,3 @@ private extension LikeOrDislikeModifier {
     }
 }
 
-extension View {
-    func likeOrDislikePhoto(photo: Binding<SharedPhoto>, showLikeAction: Binding<Bool>, size: CGFloat) -> some View {
-        modifier(LikeOrDislikeModifier(size: size, photo: photo, showLikeAction: showLikeAction))
-    }
-}
